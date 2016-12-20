a7c521b68aff2cab0d6475bd760283604a6ef610
“nerdale” <alexandra.neira@gmail.com>
2016-12-19 23:08:39 -0300

    Etapa 10: se agrega funcionalidad a la flecha para volver a index.html con windowlocation

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$('.js-menu').hide();
+
 	console.log('Ready Go!!');
 
 	$('.js-show-make').click(function(){
@@ -7,10 +8,16 @@ $(document).ready(function(){
 		$('.js-show-make').addClass('active');
 		$('.js-show-recipe').removeClass('active');
 	});
-	
+
 	$('.js-show-recipe').click(function(){
 		$('.recipe').removeClass('make');
 		$('.js-show-recipe').addClass('active');
 		$('.js-show-make').removeClass('active');
 	});
+
+	$("a#back").click(function(e){
+         e.preventDefault();
+         window.location = 'index.html';   
+	});
+
 });
