b8e1cd4bf2f2137150e97cbb0399b75529ac7b6e
“nerdale” <alexandra.neira@gmail.com>
2016-12-19 22:17:38 -0300

    Etapa 8: Se agrega funcionalidad al hacer click en script-recipes.js. Al hacer click en el elemento a se agrega y/o remueve al div contenedor la clase make

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
 	$('.js-menu').hide();
 	console.log('Ready Go!!');
 
+	$('.js-show-make').click(function(){
+		$('.recipe').addClass('make');
+	});
+
+	$('.js-show-recipe').click(function(){
+		$('.recipe').removeClass('make');
+	});
 });
