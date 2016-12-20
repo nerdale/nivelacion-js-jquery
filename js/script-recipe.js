48fcdc0426d50d0a54ac669a7a0ca639b8e5ee2d
“nerdale” <alexandra.neira@gmail.com>
2016-12-19 22:31:28 -0300

    Etapa 9: al hacer click en un elemento a se le agrega una nueva active y al mismo tiempo al otro elemento a se le remueve dicha clase

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -4,9 +4,13 @@ $(document).ready(function(){
 
 	$('.js-show-make').click(function(){
 		$('.recipe').addClass('make');
+		$('.js-show-make').addClass('active');
+		$('.js-show-recipe').removeClass('active');
 	});
-
+	
 	$('.js-show-recipe').click(function(){
 		$('.recipe').removeClass('make');
+		$('.js-show-recipe').addClass('active');
+		$('.js-show-make').removeClass('active');
 	});
 });
